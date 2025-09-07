ca7b5bf7748c96bca4115197b89916add98956a0
Daniel Schrengost <dschrengost@gmail.com>
2025-09-06 23:27:05 -0400

    fix(opt): remove JSON import assertion for Next bundler compatibility

--- lib/opt/fixtures.ts
+++ lib/opt/fixtures.ts
@@ -5,10 +5,9 @@ import { DEFAULT_FIXTURE_DATE } from "@/lib/opt/config";
 // If you change DEFAULT_FIXTURE_DATE, update this import accordingly.
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore - JSON module import typed below
-import mergedPlayers from "../../fixtures/dk/2024-01-15/mergedPlayers.json" assert { type: "json" };
+import mergedPlayers from "../../fixtures/dk/2024-01-15/mergedPlayers.json";
 
 export function loadFixtureMergedPlayers(): { date: string; players: MergedPlayer[] } {
   const arr = (mergedPlayers as any as MergedPlayer[]) ?? [];
   return { date: DEFAULT_FIXTURE_DATE, players: arr };
 }
-
